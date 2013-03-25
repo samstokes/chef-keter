530fbf5a0651dabaad81438764d5a9fd0b9d97a7
Sam Stokes <me@samstokes.co.uk>
2013-03-24 18:44:03 -0700

    use Ruby 1.8 syntax to keep chef-solo happy

--- metadata.rb
+++ metadata.rb
@@ -7,9 +7,9 @@ version     '0.0.1'
 recipe      'keter', 'Sets up Keter to run on startup'
 
 attribute   'keter/root',
-              display_name: 'Keter Root',
-              description: 'Root directory for Keter (will contain incoming/)',
-              type: 'string',
-              default: '/var/keter'
+              :display_name => 'Keter Root',
+              :description => 'Root directory for Keter (will contain incoming/)',
+              :type => 'string',
+              :default => '/var/keter'
 
 supports 'ubuntu'
